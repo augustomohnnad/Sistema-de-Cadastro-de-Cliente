7445a759a1538b05472ed3b240e32d381e43fec5
Augusto Mohannad Silveira <augustomohnnad@gmail.com>
2026-08-10 21:44:33 +0100

    Corrigido String

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const ClientModel = require('./models/clients_model');
 const ClientController = require('./controller/clients_controller');
 const ClientsRouter = require('./routes/clients_router');
 
+
 const app = express()
 app.use(express.json())
 const PORT = 3000;
@@ -12,15 +13,15 @@ const PORT = 3000;
 async function modularization(app) {
     try {
         const database = await db
-        const models = new clientModel(database)
-        const controller = new clientController(models)
+        const models = new ClientModel(database)
+        const controller = new ClientController(models)
 
-        app.use("/api/clients", clientsRouter(controller))
+        app.use("/api/clients", ClientsRouter(controller))
 
-        console.log(`Deu certoModularization completed`)
+        console.log(`Modularization completed`)
 
     } catch(e) {
-        console.error('Modularization was not possible.')
+        console.error(`[MODULARIZATION ERROR]: ${e.message} `)
     }; 
 
 
