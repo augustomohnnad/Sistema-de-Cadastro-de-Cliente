8f8480333ec63f6612f8e9c136556bdbba1f0322
Augusto Mohannad Silveira <augustomohnnad@gmail.com>
2026-08-11 22:45:55 +0100

    Finalizado CRUD no model

--- src/models/clients_model.js
+++ src/models/clients_model.js
@@ -11,6 +11,34 @@ class clientModel {
         return await this.database.exec(sql, [name_client, email, nif])
     };
 
+    updateDatabase = async (name_client, email, nif, id) => {
+        const sql = `
+            UPDATE clients SET
+            name_client = ?,
+            email = ?,
+            nif = ?
+            WHERE id = ?
+            
+        `
+        return await this.database.exec(sql, [name_client, email, nif, id]);
+    };
+
+    selectAllDatabase = async () => {
+        const sql = `SELECT *FROM clients`
+
+        return this.database.exec(sql)
+    };
+
+    selectSingleDatabase = async (id) => {
+        const sql = `SELECT *FROM clients WHERE id = ?`
+        return await this.database.exec(sql, [id])
+    }
+
+    deleteClientDatabase = async (id) => {
+        const sql = `DELETE FROM clients WHERE id = ?`
+
+        return await this.database.exec(sql, [id])
+    }
 };
 
 module.exports = clientModel;
