25219cdc6977d083abe0e304375aac725be7abec
Augusto Mohannad Silveira <augustomohnnad@gmail.com>
2026-08-19 21:26:37 +0100

    Criada rota GET por ID

--- src/routes/clients_router.js
+++ src/routes/clients_router.js
@@ -4,6 +4,9 @@ module.exports = (clientsRouter) => {
     const router = express.Router();
   
     router.post("/", (req, res) => (clientsRouter.registerCustomer(req, res)));
+    router.patch("/:id", (req, res) => (clientsRouter.editCustomer(req, res)));
+    router.get("/:id", (req, res) => (clientsRouter.getCustomer(req, res)));
+    router.get("/", (req, res) => (clientsRouter.getAllCustomer(req, res)));
 
     return router
 };
